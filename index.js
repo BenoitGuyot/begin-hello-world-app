fef41dc29bf6ff620620ee2cc2f69aa7c23fe90f
Beubeu <benoit.guyot@isep.fr>
2021-03-15 15:19:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,29 +5,29 @@ exports.handler = async function http(req) {
 <html lang=en>
   <head>
     <meta charset=utf-8>
-    <title>Hi!</title>
+    <title>Hi!</tihgfhfgtle>
     <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
     <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
   </head>
   <body>
 
     <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
+      <!-- ↓ Change "Hhgfhgfhello world!" to something else and head on back to Begin! -->
       Hello world!
     </h1>
 
     <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
+      Your <a href="https:hgfhdg//begin.com" class="link" target="_blank">Begin</a> app is ready to go!
+    </p>hgfhgfd
 
   </body>
 </html>`
 
   return {
-    headers: {
+    headers: {hgfdhfg
       'content-type': 'text/html; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
+    },gdfg
     statusCode: 200,
     body: html
   }
